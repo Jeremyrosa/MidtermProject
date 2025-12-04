4730a5023389326441ed56e6c40722c5371117b4
Elianny R. <csrodrigueze1@gmail.com>
2025-12-04 15:57:27 -0500

    login update

--- login.js
+++ login.js
@@ -2,3 +2,7 @@ document.getElementById("loginForm").addEventListener("submit", function(event) 
     let email = document.getElementById("email").value;
     let password = document.getElementById("password").value;
 });
+
+function createAccount(){
+    
+}
