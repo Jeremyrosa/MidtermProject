3b90846360c059ba1fb76f3e4dec5cb34cbf4abc
Elianny R. <csrodrigueze1@gmail.com>
2025-12-07 20:55:04 -0500

    register.php update

---
+++ register.js
@@ -0,0 +1,8 @@
+document.getElementById("regForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    let email = document.getElementById("email").value.trim();
+    let username = document.getElementById("username").value.trim();
+    let password = document.getElementById("password").value.trim();
+
+});
