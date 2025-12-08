7cbb3b95f2332c016fd0e6ade65bf86a58f042d4
Elianny R. <csrodrigueze1@gmail.com>
2025-12-07 20:56:37 -0500

    register.js update

--- register.js
+++ register.js
@@ -5,4 +5,16 @@ document.getElementById("regForm").addEventListener("submit", function(e) {
     let username = document.getElementById("username").value.trim();
     let password = document.getElementById("password").value.trim();
 
+    fetch("register.php", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            email: email,
+            username: username,
+            password: password
+        })
+    })
+
 });
